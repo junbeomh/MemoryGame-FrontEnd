7a7c0d123a44448178ab13a8c30a9b1edaf6ca27
junbeomh <junbeomh94@gmail.com>
2020-10-23 18:13:13 -0700

    modified class parameters

--- level.js
+++ level.js
@@ -1,9 +1,56 @@
 class Level {
-    constructor(obj) {
-        for (var prop in obj) {
-            this[prop] = obj[prop];
-        };
-
+    constructor() {
+        this.data = data;
+        this.currLevel = 0;
+        this.maxLevel = this.data.length;
     }
 
 }
+
+const data = [
+    {
+        x: 3,
+        y: 3,
+        answers: 3,
+    },
+    {
+        x: 4,
+        y: 3,
+        answers: 4,
+    },
+    {
+        'x': 4,
+        'y': 4,
+        answers: 5,
+    },
+    {
+        'x': 5,
+        'y': 4,
+        answers: 6,
+    },
+    {
+        'x': 5,
+        'y': 5,
+        answers: 7,
+    },
+    {
+        'x': 6,
+        'y': 5,
+        answers: 8,
+    },
+    {
+        'x': 6,
+        'y': 6,
+        answers: 9,
+    },
+    {
+        'x': 7,
+        'y': 6,
+        answers: 10,
+    },
+    {
+        'x': 7,
+        'y': 7,
+        answers: 11,
+    },
+];
