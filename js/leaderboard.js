253c43397efb633c58c0661fea2f2f7153d0af90
junbeomh <junbeomh94@gmail.com>
2020-10-28 22:08:49 -0700

    create leaderboard table dynamically with bootstrap

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,6 +1,6 @@
 function Leaderboard() {
     this.getLeaders = () => {
-        fetch(API + LEADERBOARD_ENDPOINT, {
+        fetch(LOCAL_HOST + LEADERBOARD_ENDPOINT, {
             method: "GET",
             headers: {
                 "Content-Type": "text/plain"
@@ -8,23 +8,54 @@ function Leaderboard() {
         }).then((response) => {
             return response.json();
         }).then((payload) => {
+            let leaderboard = document.getElementById('leaderboardEntries');
+            let table = document.createElement("table");
+            let tableHead = document.createElement("thead");
+            let tableBody = document.createElement("tbody");
+            let tableRow = document.createElement("tr");
+            let rankHeader = document.createElement('th');
+            let nameHeader = document.createElement('th');
+            let scoreHeader = document.createElement('th');
+            rankHeader.innerHTML = leaderboardRankHeader;
+            nameHeader.innerHTML = leaderboardNameHeader;
+            scoreHeader.innerHTML = leaderboardScoreHeader;
+            tableRow.appendChild(rankHeader);
+            tableRow.appendChild(nameHeader);
+            tableRow.appendChild(scoreHeader);
+            tableHead.appendChild(tableRow);
+            table.setAttribute('class', 'table');
+            table.appendChild(tableHead);
+            table.appendChild(tableBody);
+
             payload.forEach((data, index) => {
-                let leaderboard = document.getElementById('leaderboardEntries');
-                let entry = document.createElement('div');
-                let rank = document.createElement('p');
-                let name = document.createElement('p');
-                let score = document.createElement('p')
+                let row = document.createElement("tr");
+                let rank = document.createElement('td');
+                let name = document.createElement('td');
+                let score = document.createElement('td');
                 rank.innerHTML = `${index + 1}.`;
                 name.innerHTML = data['name'];
                 score.innerHTML = data['score'];
-                entry.setAttribute('class', 'leaderboardEntry');
-                name.setAttribute('id', 'nameLeaderBoard');
-                score.setAttribute('id', 'scoreLeaderBoard');
-                entry.appendChild(rank);
-                entry.appendChild(name);
-                entry.appendChild(score);
-                leaderboard.appendChild(entry);
+                row.appendChild(rank);
+                row.appendChild(name);
+                row.appendChild(score);
+                tableBody.appendChild(row);
+                // let entry = document.createElement('div');
+                // let rank = document.createElement('p');
+                // let name = document.createElement('p');
+                // let score = document.createElement('p')
+                // rank.innerHTML = `${index + 1}.`;
+                // name.innerHTML = data['name'];
+                // score.innerHTML = data['score'];
+                // entry.setAttribute('class', 'leaderboardEntry');
+                // name.setAttribute('id', 'nameLeaderBoard');
+                // score.setAttribute('id', 'scoreLeaderBoard');
+                // entry.appendChild(rank);
+                // entry.appendChild(name);
+                // entry.appendChild(score);
+                // leaderboard.appendChild(entry);
             })
+            leaderboard.appendChild(table);
+
         });
     }
 }
